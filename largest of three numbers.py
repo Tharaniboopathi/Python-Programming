fffafa737d24921886c562b17fd7f23c87ed565a
Tharaniboopathi <37337315+Tharaniboopathi@users.noreply.github.com>
2018-03-21 09:25:14 +0530

    Update largest of three numbers.py

--- largest of three numbers.py
+++ largest of three numbers.py
@@ -1,6 +1,6 @@
-num1 = int(input("enter an integer1))
-num2 = int(input("enter an integer2))
-num3 = int(input("enter an integer3))
+num1 = int(input("enter an integer1 :"))
+num2 = int(input("enter an integer2 :"))
+num3 = int(input("enter an integer3 :"))
 if((num1>num2) and (num1>num3)):
     print num1
 elif((num2>num3) and (num2>num1)):
